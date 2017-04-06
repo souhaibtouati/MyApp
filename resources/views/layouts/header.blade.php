641f64370551939553a448cbfc555f7b94ddd1b1
souhaibtouati <souhaib.touati@gmail.com>
2017-04-06 15:50:38 +0100

    remove adldap from json

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -90,7 +90,7 @@
                         <!-- The user image in the navbar-->
                         <img src="{{asset('/img/avatars/'.Sentinel::getUser()->avatar)}}" class="user-image" alt="User Image" />
                         <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                        <span class="hidden-xs">{{$ConnectedUser->getFullName()}} </span>
+                        <span class="hidden-xs">{{$ConnectedUser->first_name}} </span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- The user image in the menu -->
